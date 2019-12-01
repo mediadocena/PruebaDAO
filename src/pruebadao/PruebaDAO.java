2edd44f3d8db31ca735a5ac6e079f083a0016ba3
Alejandro García <45419684+mediadocena@users.noreply.github.com>
2019-12-01 14:07:12 +0100

    Pruebas done

--- src/pruebadao/PruebaDAO.java
+++ src/pruebadao/PruebaDAO.java
@@ -1,6 +1,6 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template file, choose Tools | Templates 
  * and open the template in the editor.
  */
 package pruebadao;
@@ -12,17 +12,16 @@ import java.rmi.activation.ActivationSystem;
  */
 public class PruebaDAO {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        DAOFactory conn = new SqlDbDAOFactory();
+        EmpleadoDAO emp=conn.getEmpleadoDAO();
         Empleado a = new Empleado(1,"Alejandro", 1,1, "1/1/2010", "Programmer", 21);
-        SqlDbEmpleadoImpl b = new SqlDbEmpleadoImpl();
-        b.InsertarEmp(a);
-        String consulta = b.ConsultarEmp(1).toString();
+        emp.InsertarEmp(a);
+        String consulta = emp.ConsultarEmp(1).toString();
         System.out.println(consulta);
         Empleado c = new Empleado(1,"Jesus", 2,2, "2/2/2012", "Programmer", 22);
-        b.ModificarEmp(1, c);
+        emp.ModificarEmp(1, c);
+        emp.EliminarEmp(1);
     }
     
 }
